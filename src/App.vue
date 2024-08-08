db549eabf00dc354c48f02ae711d52a8cb7fddf5
PetrHanzlik <petaxhanzlik74@gmail.com>
2024-08-08 15:42:48 +0200

    create aplication

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/"><h1>Where is the wordl?</h1></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const searchCountry = ref('')
+const region = ref('Filter by region')
+</script>
+
+<template>
+  <main>
+    <div>
+      <input type="text" v-model="searchCountry">
+      <select v-model="region">
+        <option disabled>Filter by region</option>
+        <option >Africa</option>
+        <option >America</option>
+        <option >Asia</option>
+        <option >Europe</option>
+        <option >Oceania</option>
+      </select>
+    </div>
+  </main>
+</template>
